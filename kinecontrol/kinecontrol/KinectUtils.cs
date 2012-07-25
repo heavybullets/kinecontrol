c1edc531e673dea42ceb3ce5805cdc4597719e3e
Carlos Ibáñez Ch. <dark.karurosu@gmail.com>
2012-07-25 02:09:03 -0400

    Changed mouse center to spine + shoulder

---
+++ kinecontrol/kinecontrol/KinectUtils.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Microsoft.Kinect;
+
+namespace kinecontrol
+{
+    class KinectUtils
+    {
+
+        private JointProcessor proc
+        {
+            set;
+            get;
+        }
+
+        public KinectSensor kinect
+        {
+            set;
+            get;
+        }
+
+        public KinectUtils(KinectSensor k)
+        {
+            kinect = k;
+            this.proc = new JointProcessor();
+            proc.umbral = 0.1f;
+        }
+
+        public KinectUtils()
+        {
+            this.proc = new JointProcessor();
+            proc.umbral = 0.2f;
+        }
+
+        //For Calibrating the Kinect
+        public void startCalibratingSequence()
+        {
+            //Turn off the standard skeleton frame ready
+            kinect.SkeletonFrameReady -= new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonFrameReady);
+
+            //First change the tilt angle of the Kinect to the lowest
+            kinect.ElevationAngle = (kinect.MaxElevationAngle + kinect.MinElevationAngle)/2;
+
+            //Start looking for the best position
+            kinect.SkeletonFrameReady +=new EventHandler<SkeletonFrameReadyEventArgs>(skeletonFrameCalibrateReady);
+        }
+
+        //When calibration was succesful, we resume the normal mode and store the calibrated skeleton
+        public void doneCalibratingSequence(Skeleton s)
+        {
+            this.proc.calibratedPoints = getPointsOfInterest(s);
+            this.proc.setArmLength();
+
+            kinect.SkeletonFrameReady -= new EventHandler<SkeletonFrameReadyEventArgs>(skeletonFrameCalibrateReady);
+
+            //resume normal mode
+            kinect.SkeletonFrameReady +=new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonFrameReady);
+        }
+
+        //Skeleton Frame Ready
+        void SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonFrame SFrame = e.OpenSkeletonFrame();
+
+            if (SFrame == null)
+                return; //Couldn't Open Frame
+
+            using (SFrame)
+            {
+                Skeleton[] skeletons = new Skeleton[SFrame.SkeletonArrayLength];
+                SFrame.CopySkeletonDataTo(skeletons);
+
+                SkeletonPoint[] processingPoints;
+
+                foreach (Skeleton s in skeletons)
+                {
+                    if (s.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+
+                        processingPoints = getPointsOfInterest(s);
+                        
+                        //Procesar los puntos de interes
+                        proc.processJoints(processingPoints);
+
+                    }
+                }
+
+            }   
+        }
+
+    //Skeleton Calibrate
+        public void skeletonFrameCalibrateReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            
+            SkeletonFrame SFrame = e.OpenSkeletonFrame();
+
+            if (SFrame == null)
+                return; //Couldn't Open Frame
+
+            using (SFrame)
+            {
+                Skeleton[] skeletons = new Skeleton[SFrame.SkeletonArrayLength];
+                SFrame.CopySkeletonDataTo(skeletons);
+
+                foreach (Skeleton s in skeletons)
+                {
+                    if (s.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        
+                        switch (proc.trySkeletonRange(s))
+                        {
+                            case Instruction.MOVE_UP: 
+                                //Move up
+                                kinect.ElevationAngle = (kinect.MaxElevationAngle + kinect.ElevationAngle) / 2;
+                                break;
+                            case Instruction.MOVE_DOWN: 
+                                //Move Down
+                                kinect.ElevationAngle = (kinect.ElevationAngle + kinect.MinElevationAngle) / 2;
+                                break;
+                            case Instruction.ON_RANGE: 
+                                //This one is good, stop the calibrating and record the calibrated skeleton
+                                doneCalibratingSequence(s);
+                                break;
+                        }
+                        
+                        //Need to sleep 1 second... so we don't break apart the kinect
+                        System.Threading.Thread.Sleep(1000);
+
+                    }
+                }
+            }
+
+        }
+        
+    //Get Skeleton Points
+            private SkeletonPoint[] getPointsOfInterest(Skeleton s)
+        {
+            SkeletonPoint[] sp = new SkeletonPoint[6];
+
+            //Get shoulders
+            sp[Joints.SHOULDER_L] = s.Joints[JointType.ShoulderLeft].Position;
+            sp[Joints.SHOULDER_R] = s.Joints[JointType.ShoulderRight].Position;
+
+            //Get Ankles
+            sp[Joints.ANKLE_L] = s.Joints[JointType.AnkleLeft].Position;
+            sp[Joints.ANKLE_R] = s.Joints[JointType.AnkleRight].Position;
+
+            //Wrists
+            sp[Joints.WRIST_L] = s.Joints[JointType.WristLeft].Position;
+            sp[Joints.WRIST_R] = s.Joints[JointType.WristRight].Position;
+
+<<<<<<< HEAD
+=======
+            //Shoulder Right and Left
+            sp[Joints.SPINE] = s.Joints[JointType.Spine].Position;
+
+>>>>>>> RollBack?
+            return sp;
+
+
+        }
+
+        //Stop Kinect
+        public void StopKinect(KinectSensor old)
+            {
+
+                old.Stop();
+                old.AudioSource.Stop();
+
+            }
+
+        //Start new Kinect
+        public void initNewKinect(KinectSensor newKinect)
+            {
+                //Enable the modules
+                newKinect.DepthStream.Enable();
+                newKinect.SkeletonStream.Enable();
+                //newKinect.ColorStream.Enable();
+
+                try
+                {
+                    newKinect.Start();
+                }
+                catch (System.IO.IOException) { }
+
+
+                //Add the kinect variable
+                kinect = newKinect;
+
+                newKinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonFrameReady);
+            }
+
+
+        }
+
+    }
+
+
---
+++ kinecontrol/kinecontrol/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Timers;
+using Microsoft.Kinect;
+
+namespace kinecontrol
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private KinectUtils util
+        {
+            set;
+            get;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            util = new KinectUtils();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
+
+        }
+
+        //Constructor de la kinect
+        void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KinectSensor oldK = (KinectSensor)e.OldValue;
+            KinectSensor newK = (KinectSensor)e.NewValue;
+
+            //Stop the old kinet
+            if(oldK != null)
+                util.StopKinect(oldK);
+
+            util.initNewKinect(newK);
+        }
+
+
+
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            util.StopKinect(kinectSensorChooser1.Kinect);
+        }
+
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Timer time = new Timer(5000);
+            time.AutoReset = false;
+            time.Elapsed += new ElapsedEventHandler(calibrateTimeElapsed);
+            time.Start();
+            
+        }
+
+        void calibrateTimeElapsed(object sender, ElapsedEventArgs e)
+        {
+            //Prepare for calibration
+            util.startCalibratingSequence();
+        }
+
+
+
+        //Handler for the Calibrate Button
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        }
+
+    }
+

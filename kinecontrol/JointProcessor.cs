2fe2e613f2019d0b0b43f5a25864f35e59dea42a
Carlos Ibáñez Ch. <dark.karurosu@gmail.com>
2012-07-24 22:42:42 -0400

    Initializing git repository

---
+++ kinecontrol/JointProcessor.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using System.Windows.Media.Media3D;
+using WindowsInput;
+
+//Use the inputSimulator
+
+
+namespace kinecontrol
+{
+    class JointProcessor
+    {
+        public float umbral
+        {
+            get;
+            set;
+        }
+        private SkeletonPoint[] _calibratedPoints;
+
+        public SkeletonPoint[] calibratedPoints
+        {
+            set
+            {
+                _calibratedPoints = value;
+                mouseController.rh_center = JointProcessor.transformToPoint3D(value[Joints.WRIST_R]);
+            }
+            get { return _calibratedPoints; }
+        }
+
+        //Mouse Simulator
+        private MouseController mouseController;
+
+        private float armLength;
+
+        public JointProcessor()
+        {
+            mouseController = new MouseController(MouseModes.DELTA);
+        }
+        
+        //Method for processing Joints and making events
+        public void processJoints(SkeletonPoint[] point)
+        {
+            Point3D[] diff = new Point3D[point.Length];
+
+            float x, y, z;
+
+            if (_calibratedPoints == null)
+                return;
+
+            //Get the substraction
+            for (int i = 0; i < point.Length; i++)
+            {
+                x = point[i].X - _calibratedPoints[i].X;
+                y = point[i].Y - _calibratedPoints[i].Y;
+                z = point[i].Z - _calibratedPoints[i].Z;
+
+                diff[i] = new Point3D(x, y, z);
+            }
+
+            diff = normalizeXYUsingSkeletonPoint(diff, point);
+            
+
+            //Front-Reverse Movement
+            if (-diff[Joints.SHOULDER_L].Z > umbral && -diff[Joints.SHOULDER_R].Z > umbral)
+            {
+                //See if it's pressed
+                if (InputSimulator.IsKeyDown(VirtualKeyCode.VK_W) == false)
+                    InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_W);
+            }
+            else if(InputSimulator.IsKeyDown(VirtualKeyCode.VK_W))
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_W);
+
+            //Reverse Movement
+            if (diff[Joints.SHOULDER_L].Z > umbral && diff[Joints.SHOULDER_R].Z > umbral)
+            {
+                if (InputSimulator.IsKeyDown(VirtualKeyCode.VK_S) == false)
+                    InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_S);
+            }
+            else if (InputSimulator.IsKeyDown(VirtualKeyCode.VK_S))
+                    InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_S);
+
+
+            //Jump - problems when going forward
+            if (diff[Joints.SHOULDER_L].Y > (umbral / 3) && diff[Joints.SHOULDER_R].Y > (umbral / 3))
+            {
+                //if (InputSimulator.IsKeyDown(VirtualKeyCode.SPACE) == false)
+                    InputSimulator.SimulateKeyPress(VirtualKeyCode.SPACE);
+            }
+            /*else if (InputSimulator.IsKeyDown(VirtualKeyCode.SPACE))
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.SPACE);*/
+
+            //Crouch - Problems when going back ... it thinks one is crouching
+            //TODO: Configure for type of crouching
+            if (-diff[Joints.SHOULDER_L].Y > (umbral / 3) && -diff[Joints.SHOULDER_R].Y > (umbral / 3))
+            {
+                if (InputSimulator.IsKeyDown(VirtualKeyCode.CONTROL) == false)
+                    InputSimulator.SimulateKeyDown(VirtualKeyCode.CONTROL);
+            }
+            else if (InputSimulator.IsKeyDown(VirtualKeyCode.CONTROL))
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.CONTROL);
+            
+            //Left Movement
+            if (-diff[Joints.SHOULDER_L].X > umbral && -diff[Joints.SHOULDER_R].X > umbral)
+            {
+                if (InputSimulator.IsKeyDown(VirtualKeyCode.VK_A) == false)
+                    InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_A);
+            }
+            else if (InputSimulator.IsKeyDown(VirtualKeyCode.VK_A))
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_A);
+
+            //Right Movement
+            if (diff[Joints.SHOULDER_L].X > umbral && diff[Joints.SHOULDER_R].X > umbral)
+            {
+                if (InputSimulator.IsKeyDown(VirtualKeyCode.VK_D) == false)
+                    InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_D);
+            }
+            else if (InputSimulator.IsKeyDown(VirtualKeyCode.VK_D))
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_D);
+
+            //Process Mouse Movements
+            //We give whole movements, no Deltas
+            mouseController.processHands(transformToPoint3D(point[Joints.WRIST_R]), transformToPoint3D(point[Joints.WRIST_L]));
+        }
+
+        static public Point3D normalizeXY(Point3D p, double f)
+        {
+            p.X = p.X / f;
+            p.Y = p.Y / f;
+
+            return p;
+        }
+
+        static public Point3D[] normalizeXYUsingSkeletonPoint(Point3D[] o, SkeletonPoint[] s)
+        {
+            Point3D[] sp = new Point3D[6];
+
+            //Get shoulders
+            sp[Joints.SHOULDER_L].X = o[Joints.SHOULDER_L].X / s[Joints.SHOULDER_L].Z;
+            sp[Joints.SHOULDER_L].Y = o[Joints.SHOULDER_L].Y / s[Joints.SHOULDER_L].Z;
+            sp[Joints.SHOULDER_L].Z = o[Joints.SHOULDER_L].Z;
+
+            sp[Joints.SHOULDER_R].X = o[Joints.SHOULDER_R].X / s[Joints.SHOULDER_R].Z;
+            sp[Joints.SHOULDER_R].Y = o[Joints.SHOULDER_R].Y / s[Joints.SHOULDER_R].Z;
+            sp[Joints.SHOULDER_R].Z = o[Joints.SHOULDER_R].Z;
+
+            //Get Ankles
+            sp[Joints.ANKLE_L].X = o[Joints.ANKLE_L].X / s[Joints.ANKLE_L].Z;
+            sp[Joints.ANKLE_L].Y = o[Joints.ANKLE_L].Y / s[Joints.ANKLE_L].Z;
+            sp[Joints.ANKLE_L].Z = o[Joints.ANKLE_L].Z;
+
+            sp[Joints.ANKLE_R].X = o[Joints.ANKLE_R].X / s[Joints.ANKLE_R].Z;
+            sp[Joints.ANKLE_R].Y = o[Joints.ANKLE_R].Y / s[Joints.ANKLE_R].Z;
+            sp[Joints.ANKLE_R].Z = o[Joints.ANKLE_R].Z;
+
+            //Wrists
+            sp[Joints.WRIST_L].X = o[Joints.WRIST_L].X / s[Joints.WRIST_L].Z;
+            sp[Joints.WRIST_L].Y = o[Joints.WRIST_L].Y / s[Joints.WRIST_L].Z;
+            sp[Joints.WRIST_L].Z = o[Joints.WRIST_L].Z;
+
+            sp[Joints.WRIST_R].X = o[Joints.WRIST_R].X / s[Joints.WRIST_R].Z;
+            sp[Joints.WRIST_R].Y = o[Joints.WRIST_R].Y / s[Joints.WRIST_R].Z;
+            sp[Joints.WRIST_R].Z = o[Joints.WRIST_R].Z;
+
+            return sp;
+
+        }
+
+        static public Point3D normalizeXYPoint3D(Point3D o, double s)
+        {
+            Point3D sp = new Point3D(o.X/s, o.Y/s, o.Z);
+            return sp;
+        }
+
+        public void setArmLength()
+        {
+
+            //Set arm length
+            armLength = _calibratedPoints[Joints.SHOULDER_L].Y - _calibratedPoints[Joints.ANKLE_L].Y;
+            armLength /= 2;
+        }
+
+        public int trySkeletonRange(Skeleton s)
+        {
+            if (s.Joints[JointType.Head].TrackingState == JointTrackingState.Tracked && s.Joints[JointType.FootLeft].TrackingState == JointTrackingState.Tracked
+                && s.Joints[JointType.FootRight].TrackingState == JointTrackingState.Tracked)
+            {
+                return Instruction.ON_RANGE;
+            }
+
+            //If not on range, tell where to change
+            else if (s.Joints[JointType.Head].TrackingState != JointTrackingState.Tracked)
+            {
+                return Instruction.MOVE_UP;
+            }
+            else if (s.Joints[JointType.FootRight].TrackingState != JointTrackingState.Tracked || s.Joints[JointType.FootRight].TrackingState != JointTrackingState.Tracked)
+            {
+                return Instruction.MOVE_DOWN;
+            }
+
+            return Instruction.UNDEF;
+        }
+
+        static Point3D transformToPoint3D(SkeletonPoint s)
+        {
+            double x = s.X;
+            double y = s.Y;
+            double z = s.Z;
+
+            return new Point3D(x, y, z);
+        }
+
+
+    }
+
+    //Joints Class
+    static class Joints
+    {
+
+        public const int SHOULDER_L = 0;
+        public const int SHOULDER_R = 1;
+        public const int ANKLE_L = 2;
+        public const int ANKLE_R = 3;
+        public const int WRIST_L = 4;
+        public const int WRIST_R = 5;
+
+
+    }
+
+    static class Instruction
+    {
+        public const int ON_RANGE = 0;
+        public const int MOVE_UP = 1;
+        public const int MOVE_DOWN = 2;
+        public const int UNDEF = -1;
+    }
+}
---
+++ kinecontrol/MouseController.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WindowsInputB;
+using System.Windows.Media.Media3D;
+
+namespace kinecontrol
+{
+    /// <summary>
+    /// Class that controls the mouse movements, sets the speed of movement and the direction of movement
+    /// </summary>
+    class MouseController
+    {
+        private MouseSimulator mouse;
+        private int mode
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The sensibility parameter
+        /// </summary>
+        private int sensibility
+        {
+            get;
+            set;
+        }
+
+        public double threshold
+        {
+            get;
+            set;
+        }
+
+        public Point3D rh_center
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Main Constructor, set the Mode to Delta
+        /// </summary>
+        public MouseController()
+        {
+            mouse = new MouseSimulator();
+            mode = MouseDefaults.mode;
+            sensibility = MouseDefaults.sens;
+            threshold = MouseDefaults.threshold;
+        }
+
+        /// <summary>
+        /// Main Constructor with Mode Setting
+        /// </summary>
+        /// <param name="right_hand_center">The center of the right hand</param>
+        public MouseController(int _mode)
+        {
+            mouse = new MouseSimulator();
+            mode = _mode;
+            sensibility = MouseDefaults.sens;
+            threshold = MouseDefaults.threshold;
+        }
+
+        /// <summary>
+        /// Main Constructor with Mode and Sensibility setting
+        /// </summary>
+        /// <param name="_mode"></param>
+        /// <param name="sens"></param>
+        public MouseController(int _mode, int sens)
+        {
+            mouse = new MouseSimulator();
+            mode = _mode;
+            sensibility = sens;
+            threshold = MouseDefaults.threshold;
+        }
+
+        /// <summary>
+        /// Process the Hand Movements into Mouse Actions
+        /// </summary>
+        /// <param name="rhand">Right Hand position</param>
+        /// <param name="lhand">Left Hand position</param>
+        public void processHands(Point3D rhand, Point3D lhand)
+        {
+            //TODO: sacar angulo de desfase y magnitud para obtener vector de movimiento
+
+            if (mode == MouseModes.DELTA)
+            {
+                //Obtener Deltas de movimiento
+                Point3D delta = (Point3D)(rh_center - rhand);
+
+                //Normalize
+                delta = JointProcessor.normalizeXY(delta, rhand.Z);
+                
+                //Have the delta movement, need to call the mouse
+                //We use a 15-direction based movement, ranging from -2 to +2 on the X and Y axis
+                Vector3D movement = getDeltaVector(delta);
+
+                int x = (int) Math.Round(movement.X */* movement.Z */ sensibility);
+                int y = (int)Math.Round(movement.Y * /*movement.Z */ sensibility);
+
+                mouse.MoveMouseBy(x,y);
+            }
+
+        }
+
+        /// <summary>
+        /// This methods process the delta point given transforming it into a Movement Vector, to be used by the mouse
+        /// </summary>
+        /// <param name="d">The Delta Point holding the relative movement of the hand</param>
+        /// <returns>The Delta Vector holding X and Y movements, scaled to a -2 , 2 Range each, and Z representing the Magnitude</returns>
+        public Vector3D getDeltaVector(Point3D d)
+        {
+            double x,y;
+            double abx = Math.Abs(d.X);
+            double aby = Math.Abs(d.Y); ;
+            //First see if the Vector is greater than the threshold
+            if (abx > threshold)
+            {
+
+                x = d.X - Math.Sign(d.X) * threshold;
+
+                //Check Y
+                //If it's greater see if the Y coordinate is greater, if it is, substract the treshold, if it isn't, the Y coordinate is 0
+                if (aby > threshold)
+                    y = d.Y - Math.Sign(d.Y) * threshold;
+
+                else
+                {
+                    y = 0;
+                    aby = 0;
+                }
+            }
+
+            else if (aby > threshold)
+            {
+                y = d.Y - Math.Sign(d.Y) * threshold;
+                x = 0;
+                abx = 0;
+            }
+
+            else
+            {
+                //No movement here
+                return new Vector3D(0, 0, 0);
+            }
+            
+            //TEST
+            return new Vector3D(-x, y, 0);
+            //Get the approximate magnitude
+            double temp = x * x + y * y;
+            double magnitude = Approximate.Sqrt((float)temp) * 1.5;
+            bool a;
+            //Scale the Variables to get the directions
+            if (abx > aby)
+            {
+                x = Math.Sign(x) * 2;
+                y = Math.Round(2 * y / abx);
+                if (y > 2) 
+                a = true;
+
+            }
+
+            else
+            {
+                y = Math.Sign(y) * 2;
+                x = Math.Round(2 * x / aby);
+
+                if (x > 2) 
+                    a = true;
+
+            }
+
+            //Return the vector
+            return new Vector3D(-x, y, magnitude);
+        }
+
+
+    }
+
+    static class MouseModes
+    {
+        public const int DELTA = 0;
+        public const int ABSOLUTE = 1;
+    }
+
+    static class MouseDefaults
+    {
+        public const int sens = 100;
+        public const int mode = MouseModes.ABSOLUTE;
+        public const double threshold = 0.01f;
+    }
+
+}
+
+
